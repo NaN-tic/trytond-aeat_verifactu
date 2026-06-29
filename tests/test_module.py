8bd427bc9e352b425d30a3a62a138cff7781af6f
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2026-06-29 14:37:47 +0200

    Verifactu: When accounting invoices, the PDF is created before having the QR code from the Tax Agency. (#7)
    * Do not download invoices before posting.
    
    * Sever overhaul of original proposed solution. Now invoices pdfs and qrs are still prepared beforehand, before sending them to verifactu, but they are batch handled, and sent sequentially with subsequent fingerprint signatures. An additional check exclude_verifactu has been added to journals. Also greatly improved verifactu specific function fields for invoices loadtimes, to negate the existing slowdown. Task #185531
    
    ---------
    
    Co-authored-by: Albert Cervera i Areny <albert@nan-tic.com>
    Co-authored-by: Kevin Ortega Garcia <kevin.ortega@nan-tic.com>

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,11 +1,45 @@
 # This file is part grau module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from types import SimpleNamespace
 from trytond.tests.test_tryton import ModuleTestCase
+
+from trytond.modules.aeat_verifactu.invoice import Invoice
 
 
 class GrauTestCase(ModuleTestCase):
     'Test Verifactu module'
     module = 'aeat_verifactu'
+
+    def test_build_verifactu_records_keeps_local_chain(self):
+        invoice_1 = SimpleNamespace(
+            number='INV/1',
+            verifactu_build_invoice=lambda last_line=None: {
+                'Huella': 'FP-1',
+                'PreviousFingerprint': getattr(last_line, 'fingerprint', None),
+                'PreviousInvoice': getattr(last_line.invoice, 'number', None)
+                    if last_line else None,
+            })
+        invoice_2 = SimpleNamespace(
+            number='INV/2',
+            verifactu_build_invoice=lambda last_line=None: {
+                'Huella': 'FP-2',
+                'PreviousFingerprint': getattr(last_line, 'fingerprint', None),
+                'PreviousInvoice': getattr(last_line.invoice, 'number', None)
+                    if last_line else None,
+            })
+
+        last_line = SimpleNamespace(
+            invoice=SimpleNamespace(number='INV/0'),
+            fingerprint='FP-0')
+
+        records = Invoice.build_verifactu_records(
+            [invoice_1, invoice_2],
+            last_line=last_line)
+
+        self.assertEqual(records[0]['RegistroAlta']['PreviousFingerprint'], 'FP-0')
+        self.assertEqual(records[0]['RegistroAlta']['PreviousInvoice'], 'INV/0')
+        self.assertEqual(records[1]['RegistroAlta']['PreviousFingerprint'], 'FP-1')
+        self.assertEqual(records[1]['RegistroAlta']['PreviousInvoice'], 'INV/1')
 
 del ModuleTestCase
