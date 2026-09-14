9147ba32ede7ca3970d0316581e0f785ef922c40
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2026-09-14 14:22:50 +0200

    Ported 7.8 developments into 7.6, in order to add full functionally to it, ease cherrypicking newer version, and, in general, future proofing it. Fixed several searcher functions, which were not working properly. Task #190280

--- cron.py
+++ cron.py
@@ -10,5 +10,7 @@ class Cron(metaclass=PoolMeta):
     def __setup__(cls):
         super().__setup__()
         cls.method.selection.extend([
+                ('account.invoice|cron_update_verifactu_invoices',
+                    "AEAT Verifactu Invoice Synchronization"),
                 ('account.invoice|send_verifactu', "AEAT Verifactu"),
                 ])
